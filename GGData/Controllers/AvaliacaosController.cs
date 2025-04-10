4ba623a59a84ec7d979f6e134121ab1f65aca2d7
Telmo47 <telmobotelho4@gmail.com>
2025-04-10 18:03:59 +0100

    criar controller + views

---
+++ GGData/Controllers/AvaliacaosController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GGData.Data;
+using GGData.Models;
+
+namespace GGData.Controllers
+{
+    public class AvaliacaosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AvaliacaosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Avaliacaos
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Avaliacao.Include(a => a.Jogo).Include(a => a.Usuario);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Avaliacaos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var avaliacao = await _context.Avaliacao
+                .Include(a => a.Jogo)
+                .Include(a => a.Usuario)
+                .FirstOrDefaultAsync(m => m.AvaliacaoId == id);
+            if (avaliacao == null)
+            {
+                return NotFound();
+            }
+
+            return View(avaliacao);
+        }
+
+        // GET: Avaliacaos/Create
+        public IActionResult Create()
+        {
+            ViewData["JogoID"] = new SelectList(_context.Jogo, "JogoId", "JogoId");
+            ViewData["UsuariosID"] = new SelectList(_context.Usuarios, "UsuarioId", "UsuarioId");
+            return View();
+        }
+
+        // POST: Avaliacaos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("AvaliacaoId,Nota,Comentarios,DataReview,TipoUsuario,UsuariosID,JogoID")] Avaliacao avaliacao)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(avaliacao);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["JogoID"] = new SelectList(_context.Jogo, "JogoId", "JogoId", avaliacao.JogoID);
+            ViewData["UsuariosID"] = new SelectList(_context.Usuarios, "UsuarioId", "UsuarioId", avaliacao.UsuariosID);
+            return View(avaliacao);
+        }
+
+        // GET: Avaliacaos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var avaliacao = await _context.Avaliacao.FindAsync(id);
+            if (avaliacao == null)
+            {
+                return NotFound();
+            }
+            ViewData["JogoID"] = new SelectList(_context.Jogo, "JogoId", "JogoId", avaliacao.JogoID);
+            ViewData["UsuariosID"] = new SelectList(_context.Usuarios, "UsuarioId", "UsuarioId", avaliacao.UsuariosID);
+            return View(avaliacao);
+        }
+
+        // POST: Avaliacaos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("AvaliacaoId,Nota,Comentarios,DataReview,TipoUsuario,UsuariosID,JogoID")] Avaliacao avaliacao)
+        {
+            if (id != avaliacao.AvaliacaoId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(avaliacao);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AvaliacaoExists(avaliacao.AvaliacaoId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["JogoID"] = new SelectList(_context.Jogo, "JogoId", "JogoId", avaliacao.JogoID);
+            ViewData["UsuariosID"] = new SelectList(_context.Usuarios, "UsuarioId", "UsuarioId", avaliacao.UsuariosID);
+            return View(avaliacao);
+        }
+
+        // GET: Avaliacaos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var avaliacao = await _context.Avaliacao
+                .Include(a => a.Jogo)
+                .Include(a => a.Usuario)
+                .FirstOrDefaultAsync(m => m.AvaliacaoId == id);
+            if (avaliacao == null)
+            {
+                return NotFound();
+            }
+
+            return View(avaliacao);
+        }
+
+        // POST: Avaliacaos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var avaliacao = await _context.Avaliacao.FindAsync(id);
+            if (avaliacao != null)
+            {
+                _context.Avaliacao.Remove(avaliacao);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AvaliacaoExists(int id)
+        {
+            return _context.Avaliacao.Any(e => e.AvaliacaoId == id);
+        }
+    }
+}
