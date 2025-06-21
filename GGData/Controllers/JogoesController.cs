173b91058c598eb79889b1aadc9066ec873f561e
Telmo47 <telmobotelho4@gmail.com>
2025-06-21 16:41:43 +0100

    Criação do controller jogo e update da DB

---
+++ GGData/Controllers/JogoesController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GGData.Data;
+using GGData.Models;
+
+namespace GGData.Controllers
+{
+    public class JogoesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public JogoesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Jogoes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Jogo.ToListAsync());
+        }
+
+        // GET: Jogoes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var jogo = await _context.Jogo
+                .FirstOrDefaultAsync(m => m.JogoId == id);
+            if (jogo == null)
+            {
+                return NotFound();
+            }
+
+            return View(jogo);
+        }
+
+        // GET: Jogoes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Jogoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("JogoId,Nome,Genero,Plataforma,DataLancamento")] Jogo jogo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(jogo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(jogo);
+        }
+
+        // GET: Jogoes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var jogo = await _context.Jogo.FindAsync(id);
+            if (jogo == null)
+            {
+                return NotFound();
+            }
+            return View(jogo);
+        }
+
+        // POST: Jogoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("JogoId,Nome,Genero,Plataforma,DataLancamento")] Jogo jogo)
+        {
+            if (id != jogo.JogoId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(jogo);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!JogoExists(jogo.JogoId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(jogo);
+        }
+
+        // GET: Jogoes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var jogo = await _context.Jogo
+                .FirstOrDefaultAsync(m => m.JogoId == id);
+            if (jogo == null)
+            {
+                return NotFound();
+            }
+
+            return View(jogo);
+        }
+
+        // POST: Jogoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var jogo = await _context.Jogo.FindAsync(id);
+            if (jogo != null)
+            {
+                _context.Jogo.Remove(jogo);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool JogoExists(int id)
+        {
+            return _context.Jogo.Any(e => e.JogoId == id);
+        }
+    }
+}
