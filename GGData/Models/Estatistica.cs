2b0b0badafd614be0c378cb0b17d95d14ab33fba
Telmo47 <telmobotelho4@gmail.com>
2025-06-22 16:38:34 +0100

    Alteração no model das estaticas para introdução das notas dos jogos

--- GGData/Models/Estatistica.cs
+++ GGData/Models/Estatistica.cs
@@ -3,13 +3,11 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GGData.Models
 {
-
     /// <summary>
     /// Estatísticas de cada jogo
     /// </summary>
     public class Estatistica
     {
-
         /// <summary>
         /// Id das estatísticas do jogo
         /// </summary>
@@ -19,23 +17,29 @@ namespace GGData.Models
         /// <summary>
         /// Conquistas obtidas
         /// </summary>
-
         public string Conquistas { get; set; }
 
         /// <summary>
         /// Tempo médio de jogo atribuido pelos utilizadores
         /// </summary>
-
         public decimal TempoMedioJogo { get; set; }
 
         /// <summary>
         /// Número total de avaliações
         /// </summary>
-
         public int TotalAvaliacoes { get; set; }
 
-        //FKs
+        /// <summary>
+        /// Média das notas dadas pelos utilizadores comuns
+        /// </summary>
+        public decimal MediaNotaUtilizadores { get; set; }
 
+        /// <summary>
+        /// Média das notas dadas pelos críticos
+        /// </summary>
+        public decimal MediaNotaCriticos { get; set; }
+
+        // FK
 
         /// <summary>
         /// Chave forasteira com referência ao Jogo
