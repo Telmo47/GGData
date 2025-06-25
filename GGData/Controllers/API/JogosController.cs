e4e199161f0a12ae4822700e0bd0829a2556a81b
Telmo47 <telmobotelho4@gmail.com>
2025-06-25 15:55:57 +0100

    Adicionada API de Jogos e DTO de Jogo para acesso via REST

---
+++ GGData/Controllers/API/JogosController.cs
@@ -0,0 +1,36 @@
+﻿using GGData.Data;
+using GGData.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GGData.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JogosController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public JogosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Jogo>>> GetJogos()
+        {
+            return await _context.Jogo.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Jogo>> GetJogo(int id)
+        {
+            var jogo = await _context.Jogo.FindAsync(id);
+
+            if (jogo == null)
+                return NotFound();
+
+            return jogo;
+        }
+    }
+}
